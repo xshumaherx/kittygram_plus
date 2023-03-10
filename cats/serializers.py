e1ee7d9dc5397fe1c32209542ed5909ff9cf23c9
Вдовенко Александр <Shumaher2014@mail.ru>
2023-03-10 17:59:00 +0300

    recent-white-cats

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,75 @@
+import datetime as dt
+
+import webcolors
 from rest_framework import serializers
 
-from .models import Cat
+from .models import Achievement, AchievementCat, Cat, Owner, CHOICES
+
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+    
+    class Meta:
+        model = Achievement
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
-
+    achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = serializers.ChoiceField(choices=CHOICES)
+    #color = Hex2NameColor()  # Вот он - наш собственный тип поля
+    
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
+    
+    def create(self, validated_data):
+        if 'achievements' not in self.initial_data:
+        # Создадим нового котика пока без достижений, данных нам достаточно
+            cat = Cat.objects.create(**validated_data)
+            return cat 
+        # Уберем список достижений из словаря validated_data и сохраним его
+        achievements = validated_data.pop('achievements')
+        # Создадим нового котика пока без достижений, данных нам достаточно
+        cat = Cat.objects.create(**validated_data)
+        # Для каждого достижения из списка достижений
+        for achievement in achievements:
+            # Создадим новую запись или получим существующий экземпляр из БД
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement)
+            # Поместим ссылку на каждое достижение во вспомогательную таблицу
+            # Не забыв указать к какому котику оно относится
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat)
+        return cat 
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year 
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
+
+
+
--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,25 @@
 from rest_framework import viewsets
-
-from .models import Cat
-from .serializers import CatSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Cat, Owner
+from .serializers import CatSerializer, OwnerSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    # Пишем метод, а в декораторе разрешим работу со списком объектов
+    # и переопределим URL на более презентабельный
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        # Нужны только последние пять котиков белого цвета
+        cats = Cat.objects.filter(color='White')[:5]
+        # Передадим queryset cats сериализатору 
+        # и разрешим работу со списком объектов
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data) 
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer 
